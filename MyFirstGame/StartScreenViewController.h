6f67b379ebe1da25aafa35401ee6aff47a97761f
stfnjstn <stfn.jstn@gmail.com>
2014-02-13 23:33:43 +0100

    Post 1 Sample Code
    Sample project with a start screen to demonstrate parallax behavior

---
+++ MyFirstGame/StartScreenViewController.h
@@ -0,0 +1,15 @@
+//
+//  StartScreenViewController.h
+//  MyFirstGame
+//
+//  Created by STEFAN JOSTEN on 10.02.14.
+//  Copyright (c) 2014 Stefan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StartScreenViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIImageView *backgroundView;
+@property (strong, nonatomic) IBOutletCollection(UIView) NSArray *foregroundViews;
+
+@end
